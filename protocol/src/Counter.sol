68b00196cec859039906a3dbfda187d4f5158f70
lazaralex98 <alex2012ro@gmail.com>
2024-10-15 04:19:59 +0300

    add event for counting

--- protocol/src/Counter.sol
+++ protocol/src/Counter.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
+    event NumberSet(uint256 newNumber, address indexed by);
+
     function increment() public {
         number++;
+        emit NumberSet(number, msg.sender);
     }
 }
